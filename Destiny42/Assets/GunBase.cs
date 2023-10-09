ea764b325699ea6269488bc199a59329b84b3eb7
Simon <mukopper@gmail.com>
2023-10-09 15:50:42 +0200

    Initiated Gun Maintenance
    Producing gun

--- Destiny42/Assets/GunBase.cs
+++ Destiny42/Assets/GunBase.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FishNet.Connection;
+using FishNet.Object;
 
-public class GunBase : MonoBehaviour
+public class GunBase : NetworkBehaviour
 {
-    // Start is called before the first frame update
+    public int gunDamage;
+
+    public float fireRate;
+
+    public float weaponRange;
+
+    public float hitForce;
+
+    public Transform gunEnd;
+
+
+
+
+
+    private Camera fpsCam;
+
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+
+    private AudioSource gunAudio;
+
+    private LineRenderer laserLine;
+
+    private float nextFire;
+
+
+
     void Start()
+
     {
-        
+
+        laserLine = GetComponentInChildren<LineRenderer>();
+
+        gunAudio = GetComponent<AudioSource>();
+
+        fpsCam = GetComponent<Camera>();
+
     }
 
-    // Update is called once per frame
     void Update()
+
     {
-        
+
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+           // *Not implemented* StartCoroutine(ShotEffect());
+
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        }
+
     }
+
 }
+
