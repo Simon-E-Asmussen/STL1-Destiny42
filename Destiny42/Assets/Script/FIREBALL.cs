0722895ccf23fabf20087e812f6ee548f94078fd
Simon <mukopper@gmail.com>
2023-11-23 13:31:12 +0100

    Initated fixed shit
    I cast mending

--- Destiny42/Assets/Script/FIREBALL.cs
+++ Destiny42/Assets/Script/FIREBALL.cs
@@ -15,22 +15,30 @@ public class FIREBALL : NetworkBehaviour
     SphereCollider col;
     public GameObject myself;
     int damage = 5;
+    float cd;
 
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.LogWarning("I started");
-
+        cd = 1.5f;
         //Finds collider and initiates the particle system
         col = GetComponent<SphereCollider>();
         this.GetComponent<ParticleSystem>().Play();
-    
+        col.enabled = !col.enabled;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(cd != 0f)
+        {
+            cd -= Time.deltaTime;
+        }else
+        {
+            col.enabled = col.enabled;
+        }
         Debug.LogWarning("I updated");
         //Moves the fireball toward the target and initiates the collider. Collider is disabled by default to prevent it from exploding on launch.
         if (recieved && !hasGone)
@@ -55,23 +63,26 @@ public class FIREBALL : NetworkBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.LogWarning("Trigger collision");
-        if (hasGone)
+        if (other.name != "FPSlayer(clone)")
         {
-            
-            
-            this.transform.localScale = (new Vector3(8,8,8));
-            Explode();
-        }
-        else if (!hasGone)
-        {
-            hasGone = true;
-            target = this.transform.position;
-        } 
+            if (hasGone)
+            {
 
-        if(other.GetComponent<ShootableTarget>() != null)
-        {
-            other.GetComponent<ShootableTarget>().Damage(damage);
 
+                this.transform.localScale = (new Vector3(8, 8, 8));
+                Explode();
+            }
+            else if (!hasGone)
+            {
+                hasGone = true;
+                target = this.transform.position;
+            }
+
+            if (other.GetComponent<ShootableTarget>() != null)
+            {
+                other.GetComponent<ShootableTarget>().Damage(damage);
+
+            }
         }
     }
     IEnumerator Wait()
@@ -89,7 +100,7 @@ public class FIREBALL : NetworkBehaviour
     void Explode()
     {
         Debug.LogWarning("I blew up");
-        // Deal Damage
+       
 
         Destroy(this.gameObject);
     }
